48c509aa50b21a71963aa1221b28a3cbc0843477
Dove. J. Kim <kimhippowork@gmail.com>
2023-04-20 13:23:53 +0900

    ['23.04.20][RENAME] 폴더 구성 변경

---
+++ src/bot.py
@@ -0,0 +1,27 @@
+import json
+
+from discord.ext import commands
+import discord
+
+from misc import loggers, configs, rcon
+
+ROOT_PATH = configs.ROOT_PATH
+LOGGER    = loggers.logger
+CONFIG    = configs.CONFIG
+TOKEN     = CONFIG.bot_token
+
+app = commands.Bot(command_prefix = '/')
+@app.event
+async def on_ready():
+
+    LOGGER.info('Bot ready')
+    await app.change_presence(status   = discord.Status.online, activity = None)
+
+
+@app.command()
+async def hello(ctx):
+    await ctx.send('안녕하세요. 저는 전서구 입니다.')
+
+
+app.run(TOKEN)
+
---
+++ src/misc/configs.py
@@ -0,0 +1,8 @@
+import json
+
+from easydict import EasyDict as edict
+
+ROOT_PATH   = '/config/workspace/project'
+_CONFIG_PATH = f'{ROOT_PATH}/utils/configs.json'
+CONFIG      = json.loads(open(_CONFIG_PATH, 'r').read())
+CONFIG      = edict(CONFIG)
