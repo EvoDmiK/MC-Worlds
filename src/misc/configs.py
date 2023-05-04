764e97a13c45e4b47ae0decac9d61e42e1723ce0
Dove. J. Kim <kimhippowork@gmail.com>
2023-05-04 16:53:53 +0900

    ['23.05.04][PERF] config 파일 복구 알고리즘 수정

--- src/misc/configs.py
+++ src/misc/configs.py
@@ -8,57 +8,61 @@ from misc import logger
 
 ROOT_PATH        = '/config/workspace/project'
 DATA_PATH        = f'{ROOT_PATH}/MC-Worlds/src/data/jsons'
-JSON_PATH        = f'{ROOT_PATH}/utils/'
+JSON_PATH        = f'{ROOT_PATH}/utils/configs'
 JSON_BACKUP_PATH = f'{ROOT_PATH}/BACKUPS/configs'
-
-CONFIG_NAMES     = [   'port',   'host',  'passwd', 'api_host',
-                    'youtube',  'steam', 'openai',
-                    'mrcon_api_int_port',
-                    'mrcon_api_ext_port',
-                    'chimhaha_int_port', 
-                    'chimwiki_int_port', 
-                    'dovenest_int_port', 
-                    'dovenest_api_int_port']
-
-LOGGER       = logger.get_logger()
+LOGGER           = logger.get_logger()
 
 ## json 파일을 불러와주는 함수
 load_json     = lambda json_path: json.loads(open(json_path, 'r').read())
 
 ## json 파일을 저장하는 함수
-save_json     = lambda data, json_path: json.dump(data, open(json_path, 'w'))
+save_json     = lambda data, json_path: json.dump(data, open(json_path, 'w'), indent = 2)
 
-def repair_config(json_path):
+def repair_json(json_path, ftype = 'config'):
 
-    try: config = load_json(f'{json_path}/config.json')
+    try: json_ = load_json(f'{json_path}/{ftype}.json')
 
     except Exception as e:
 
-        LOGGER.error(f'json 파일이 깨져 열 수 없습니다. {e} \n{format_exc()}')
-        texts = open(f'{JSON_BACKUP_PATH}/config.txt', 'r').read().split('\n')
+        LOGGER.error(f'json 파일이 깨져 열 수 없습니다. {e} \n')
+        texts = open(f'{JSON_BACKUP_PATH}/{ftype}.txt', 'r').readlines()
 
         texts  = [text.split() for text in texts]
-        texts  = [text for text in texts if text != []]
+        json_  = {name : text for name, text in texts}
 
-        config = {name : text for name, text in texts}
         save_paths = [JSON_PATH, JSON_BACKUP_PATH]
         for save_path in save_paths:
 
             os.makedirs(save_path, exist_ok = True)
-            save_json(config, f'{save_path}/config.json')
+            save_json(json_, f'{save_path}/{ftype}.json')
 
     
-    finally: return config
+    finally: return json_
 
 def get_config():
 
     if os.path.isfile(f'{JSON_PATH}/config.json'):
-        config = repair_config(JSON_PATH)
+        config = repair_json(JSON_PATH)
 
     else:
         LOGGER.warning('json 파일이 존재하지 않아 백업 데이터를 로딩합니다.')
-        config = repair_config(JSON_BACKUP_PATH)
+        config = repair_json(JSON_BACKUP_PATH)
 
     return edict(config)
 
+
+def get_ports():
+
+    if os.path.isfile(f'{JSON_PATH}/ports.json'):
+
+        ports = repair_json(JSON_PATH, ftype = 'ports')
+
+    else:
+        LOGGER.warning('json 파일이 존재하지 않아 백업 데이터를 로딩합니다.')
+        ports = repair_json(JSON_BACKUP_PATH, ftype = 'ports')
+
+    return edict(ports)
+
+
 CONFIG = get_config()
+PORTS  = get_ports()
