9ccf8f88f9d91eaf8ae3c944ea9f884aeae5bf0f
Dove. J. Kim <kimhippowork@gmail.com>
2023-05-11 17:05:05 +0900

    ['23.05.11][REFAC]현재 접속 되어있는 플레이어 정보 조회 등 기능 리팩토링

--- src/misc/rcon.py
+++ src/misc/rcon.py
@@ -7,21 +7,27 @@ import re
 
 ## 써드파티 모듈
 from mcrcon import MCRcon as mrcon
+from redis import Redis
 
 ## 구현 모듈
-from misc import configs, logger
+from misc import configs
 
 ## 테스트 코드
 # import configs, loggers
 
 ROOT_PATH = configs.ROOT_PATH
-SAVE_PATH = configs.DATA_PATH
+DATA_PATH = configs.DATA_PATH
+
+RHOST     = configs.CONFIG.global_host
+RPORT     = configs.PORTS.redis_port
+rconn     = Redis(host = RHOST, port = RPORT, 
+                  db = 2, decode_responses = True)
 
 class RconInterface:
+    ## 클래스의 생성자
+    def __init__(self, host: str, port: int, password: str):
 
-    def __init__(self, host, port, password):
-
-        self.LOGGER = logger.get_logger()
+        self.LOGGER = configs.LOGGER
 
         ## RCON 서버에 접속해 주는 부분 (이게 되야 모든 코드가 돌아감,, 흑흑)
         self.con    = mrcon(host = host, port = port, password = password)
@@ -31,6 +37,7 @@ class RconInterface:
         except Exception as e:
             self.LOGGER.error(f'서버에 연결 할 수 없었습니다. 설정값을 확인해주세요.')
             self.LOGGER.error(format_exc())
+
 
     ## RCON에 명령어 전달해주는 함수
     def _command(self, command: str) -> str:
@@ -61,29 +68,20 @@ class RconInterface:
         return [re.sub('[^a-zA-Z0-9]', '', data[0]), data[1].strip()]
 
 
+    ## 현재 접속해 있는 플레이어 정보를 가져와주는 함수
+    def _get_players(self) -> list:
+        return [player for player in self._command('list') if player != '']
+
+
     ## 현재 접속 중인 플레이어 수와 닉네임을 불러와주는 함수
     def num_players(self) -> int:
         
         ## list 명령어를 통해서 서버에 들어와 있는 인원 수와 이름 정보 가져오기
-        players = self._command('list')
-        players = [player for player in players if player != '']
-
+        players     = self._get_players()
         num_players = players[0]
-        num_players  = int(self._only_digits(num_players)) 
+        num_players = int(self._only_digits(num_players)) 
 
-        clear_data = lambda player: player.replace(' ', '').replace('[AFK]', '').split(':')[1].split(',')
-
-        ## 누군가 서버에 들어와 있는 경우
-        if num_players != 0:
-            players = players[1:]
-            players = [clear_data(player) for player in players]
-
-            return num_players, players
-
-        ## 서버에 아무도 없는 경우
-        else:
-            self.LOGGER.info('현재 접속 중인 플레이어가 없습니다. 나중에 다시 요청바랍니다.')
-            return num_players
+        return num_players
 
 
     ## 플레이어 정보를 불러오는 함수
@@ -95,34 +93,42 @@ class RconInterface:
         infos = {k : v for k, v in infos}
 
         try:
-            infos = infos if infos != {} else json.loads(open(f'{SAVE_PATH}/{player}.json', 'r').read())
+            ## 서버에 플레이어가 접속해 있지 않은 경우
+            if infos == {}:
+                json_path = f'{DATA_PATH}/json/{player}.json'
+                
+                ## redis에 존재하는 플레이어 정보 로딩
+                try: infos  = json.loads(rconn.get(player))
 
-        except:
-            msg   = f'현재 {player}가 접속해 있지 않거나 {player}의 정보가 저장되어 있는 파일이 존재 하지 않습니다.'
+                ## redis에 플레이어의 정보가 저장되어 있지 않은 경우
+                ## 데이터 폴더에 저장되어 있는 플레이어 json 파일 로딩
+                except:
+                    self.LOGGER.error(f'[ERR.DB.Red.D-0001]현재 {player}의 정보가 저장되어 있는 파일이 존재하지 않습니다.')
+                    infos = json.loads(open(json_path, 'r').read())
+
+            ## 데이터를 처음 저장할때 마지막 로그인 시간을 현재로 저장하도록 설정
+            infos['latest_online'] = infos['latest_online'] \
+                                     if 'latest_online' in infos.keys() else time.time()
+
+        except Exception as e:
+            msg   = f'현재 {player}가 접속해 있지 않거나 {player}의 정보가 저장되어 있는 파일이 존재하지 않습니다.'
             infos = {'Nick' : msg}
-            self.LOGGER.error(msg)
+            self.LOGGER.info(msg)
+            self.LOGGER.error(e)
 
         return infos
 
 
-    
-    ## 플레이어 정보를 저장하는 함수
-    def save_info(self):
+    ## 현재 접속해 있는 플레이어들의 닉네임을 가져오는 함수
+    def online_players(self) -> list:
 
-        try:
-            _, players = self.num_players()
-            
-            for player in players:
-                player_info = self.who_is(player)
+        clear_name  = lambda player: player.replace(' ', '').replace('[AFK]', '').split(':')[1].split(',')
+        players     = self._get_players()
+        
+        players = players[1:]
+        players = [clear_name(player) for player in players]
 
-                os.makedirs(SAVE_PATH, exist_ok = True)
-                with open(f'{SAVE_PATH}/{player}.json', 'w') as f:
-                    json.dump(player_info, f, indent = 2)
-
-        except:
-            self.LOGGER.info('현재 접속 중인 플레이어가 없습니다. 나중에 다시 요청바랍니다.')
-            self.LOGGER.info(format_exc())
-
+        return players
 
 
 if __name__ == '__main__':
